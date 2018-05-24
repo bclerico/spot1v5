b82e65bc360762dfd502b87317f1e93981b8551f
Bill Clerico <bclerico@gmail.com>
2018-05-23 21:41:07 -0400

    RVR Modifications

--- html/header.php
+++ html/header.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h1>Watch My Spot - Global Expedition Observation Portal</h1>"
+echo "<h1>Watch My Spot @ Raven View Ranch</h1>"
 ?>
---
+++ sql/spot_home_02.sql
@@ -0,0 +1,4 @@
+LOCK TABLES `content` WRITE;
+INSERT INTO `content` (cnkey, cnhtml, dto)
+  VALUES ('home_02','<h1>Welcome to Raven View Ranch!</h1><p>The Console page shows various tags that people have logged while roaming about RVR</p>','20180523113039');
+UNLOCK TABLES;
